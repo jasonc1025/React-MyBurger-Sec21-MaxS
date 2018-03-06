e368d50033dc3675405aaa3b7006654365a66047
jasonc1025 <jasonc@eande.world>
2018-03-05 19:44:12 -0800

    Firebase: Customization: Authentication

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -48,9 +48,12 @@ export const auth = (email, password, isSignup) => {
             password: password,
             returnSecureToken: true
         };
-        let url = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyB5cHT6x62tTe-g27vBDIqWcwQWBSj3uiY';
+        
+        // [jwc] let url = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyB5cHT6x62tTe-g27vBDIqWcwQWBSj3uiY';
+        let url = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyDjNGyfqCbmmY7r7eZYDXxIaxjYa4L1nyA';
         if (!isSignup) {
-            url = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=AIzaSyB5cHT6x62tTe-g27vBDIqWcwQWBSj3uiY';
+            // [jwc] url = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=AIzaSyB5cHT6x62tTe-g27vBDIqWcwQWBSj3uiY';
+            url = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=AIzaSyDjNGyfqCbmmY7r7eZYDXxIaxjYa4L1nyA';
         }
         axios.post(url, authData)
             .then(response => {
